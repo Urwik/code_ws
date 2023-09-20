9ee956c6a4470dbf30975f81ce4ae58046035397
Fran Soler <f.soler@umh.es>
2023-09-20 14:02:29 +0200

    fix: plane detection feat: plot in colors

--- src/truss_plane_detector.cpp
+++ src/truss_plane_detector.cpp
@@ -1,16 +1,12 @@
-#include <pcl/common/common.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
 #include "arvc_utils.hpp"
 
 #include <ignition/math/Matrix4.hh>
 #include <ignition/math.hh>
-#include <eigen3/Eigen/Dense>
+#include <pcl/pcl_config.h>
 
 
 namespace im = ignition::math;
 namespace eig = Eigen;
-
 
 
 class truss_plane_detector
@@ -33,6 +29,7 @@ public:
     arvc::plane plane;
 };
 
+// DEFAULT CONSTRUCTOR
 truss_plane_detector::truss_plane_detector()
 {
     this->cloud_in_xyz.reset(new pcl::PointCloud<pcl::PointXYZ>);
@@ -40,9 +37,9 @@ truss_plane_detector::truss_plane_detector()
     this->QSensorToTruss = im::Pose3d::Zero;
     this->min_pt = Eigen::Vector4f::Zero();
     this->max_pt = Eigen::Vector4f::Zero();
-    this->plane = arvc::plane();
 }
 
+// CONSTRUCTOR
 truss_plane_detector::truss_plane_detector(double range_dist, im::Pose3d QSensorToTruss)
 {
     this->cloud_in_xyz.reset(new pcl::PointCloud<pcl::PointXYZ>);
@@ -50,18 +47,13 @@ truss_plane_detector::truss_plane_detector(double range_dist, im::Pose3d QSensor
     this->QSensorToTruss = QSensorToTruss;
     this->min_pt << -range_dist, -range_dist, -range_dist, 1.0;
     this->max_pt << range_dist, range_dist, range_dist, 1.0;
-    this->plane = arvc::plane();
 }
 
-
+// DESTRUCTOR
 truss_plane_detector::~truss_plane_detector()
 {
-    this->cloud_in_xyz.reset(new pcl::PointCloud<pcl::PointXYZ>);
-    this->cloud_search_xyz.reset(new pcl::PointCloud<pcl::PointXYZ>);
-    this->QSensorToTruss = im::Pose3d::Zero;
-    this->min_pt = Eigen::Vector4f::Zero();
-    this->max_pt = Eigen::Vector4f::Zero();
-    this->plane = arvc::plane();
+    this->cloud_in_xyz.reset();
+    this->cloud_search_xyz.reset();
 }
 
 
@@ -72,19 +64,15 @@ void truss_plane_detector::detect_plane(){
     pcl::ModelCoefficientsPtr plane_coeffs (new pcl::ModelCoefficients);
 
     ransac.setInputCloud(this->cloud_search_xyz);
-    ransac.setOptimizeCoefficients(true);
+    ransac.setOptimizeCoefficients(false);
     ransac.setModelType(pcl::SACMODEL_PLANE);
     ransac.setMethodType(pcl::SAC_RANSAC);
     ransac.setMaxIterations(1000);
-    ransac.setDistanceThreshold(0.1);
+    ransac.setDistanceThreshold(0.02);
     ransac.segment(*point_indices, *plane_coeffs);
-
-    this->plane = arvc::plane(plane_coeffs, point_indices);
-
-    cout << "DETECTED PLANE COEFFS: " << *this->plane.coeffs << endl;
-    cout << "DETECTED PLANE INLIERS: " << this->plane.inliers->indices.size() << endl;
-    cout << "------------------------" << endl;
-
+    
+    *this->plane.coeffs = *plane_coeffs;
+    *this->plane.inliers = *point_indices;
 }
 
 
@@ -101,7 +89,6 @@ void truss_plane_detector::get_close_points(){
     extract.setNegative(false);
     extract.filter(*this->cloud_search_xyz);
 
-    cout << "CLOUD SEARCH SIZE: " << this->cloud_search_xyz->points.size() << endl;
 }
 
 
@@ -122,7 +109,43 @@ int main(int argc, char const *argv[])
 
     PointCloud::Ptr plane_cloud = td.plane.getCloud(td.cloud_search_xyz);
 
-    arvc::visualizeClouds(td.cloud_search_xyz, plane_cloud);
+    // arvc::visualizeClouds(td.cloud_search_xyz, plane_cloud);
+
+    pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+
+    viewer->setBackgroundColor(0.5, 0.5, 0.5);
+    viewer->addCoordinateSystem(0.50, "global");
+
+    Eigen::Vector3f position(0.0, 0.0, 0.0);
+    pcl::PointXYZ position_point(0,0,0);
+
+    // viewer->addCube(position, td.cloud_search_xyz->sensor_orientation_, 0.085, 0.085, 0.073, "cube");
+    viewer->addSphere(position_point, 0.05, "sphere");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1.0, 1.0, 0.0, "sphere");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, "sphere");
+
+    viewer->addPointCloud<pcl::PointXYZ>(td.cloud_search_xyz, "cloud");
+    pcl::PointXYZ centroid;
+    pcl::computeCentroid(*plane_cloud, centroid);
+
+    pcl::PointXYZ plane_normal;
+    plane_normal.x = td.plane.coeffs->values[0] + centroid.x;
+    plane_normal.y = td.plane.coeffs->values[1] + centroid.y;
+    plane_normal.z = td.plane.coeffs->values[2] + centroid.z;
+
+
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> plane_color(plane_cloud, 0, 255, 0);
+    viewer->addPointCloud<pcl::PointXYZ>(plane_cloud, plane_color, "plane");
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "plane");
+    viewer->addArrow<pcl::PointXYZ, pcl::PointXYZ>(centroid, plane_normal, 0.2, 0.2, 0.2, "normal");
+    
+
+    while (!viewer->wasStopped())
+    {
+        viewer->spinOnce(100);
+    }
+
+
 
     return 0;
 }
